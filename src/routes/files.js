5689b86aab7417979edf9e6442cac92868ed460d
b-rad-c <bradcorlett@gmail.com>
2022-03-08 15:48:01 -0800

    finished list ipfs files

--- src/routes/files.js
+++ src/routes/files.js
@@ -4,38 +4,41 @@ import { PencilSquare } from 'react-bootstrap-icons'
 import { Link } from 'react-router-dom';
 import { create } from 'ipfs-http-client'
 
-const ipfsClient = create('http://127.0.0.1:5001')
 
-async function loader() {
-    for await (const x of ipfsClient.files.ls('/dBranch/published')) {
-        console.log(x);
-    }
-}
 
-loader()
+
+
+
 
 
 export default function FilesPage(props) {
     const settings = props.settings
     const [loading, setLoading] = useState(true)
     const [drafts, setDrafts] = useState([])
+    const [publishedDocs, setPublishedDocs] = useState([])
     const [errorMsg, setErrorMsg] = useState('')
 
-    useEffect(() => {
+    async function loadIPFSDocs() {
+        console.log('listing published documents...')
+        const names = []
         const ipfsClient = create(settings.ipfsHost)
-        console.log(ipfsClient.files.ls(settings.dBranchPublishedDir))
+        for await (const f of ipfsClient.files.ls('/dBranch/published')) {
+            names.push(f.name)
+        }
+        return names
+    }
 
-        /*(async () => {
-            for await (const x of ipfsClient.files.ls(settings.dBranchPublishedDir)) {
-                console.log(x);
-            }
-        })();*/
-
+    useEffect(() => {
+        loadIPFSDocs()
+            .then((result) => {
+                console.log('ipfs files', result)
+                setPublishedDocs(result)
+            })
     // eslint-disable-next-line
     }, [])
 
     useEffect(() => {
-        console.log('listing user documents')
+        console.log('listing user documents...')
         window.dBranch.listUserDocuments()
             .then((result) => {
                 const docs = result.filter((name) => !name.startsWith('.') )
@@ -56,11 +59,25 @@ export default function FilesPage(props) {
             <p className='alert-text'>{errorMsg}</p>
         </Alert>
         <div className='content'>
-            <p className='inline-header'><strong>published :: </strong>0 {label}</p>
+            <p className='inline-header'><strong>published :: </strong>{publishedDocs.length} {label}</p>
+            {publishedDocs.length === 0 &&<p>no files found</p>}
+            {loading && <Spinner />}
+            <Container>
+                {   publishedDocs.map((publishedDoc, index) => {
+                        return (
+                            <Row key={index}>
+                                <Col>
+                                    {publishedDoc}
+                                </Col>
+                            </Row>
+                        )
+                    })
+                }
+            </Container>
         </div>
         <div className='content'>
             <p className='inline-header'><strong>drafts :: </strong>{drafts.length} {label}</p>
-            {drafts.length === 0 &&<span>no files found</span>}
+            {drafts.length === 0 &&<p>no files found</p>}
             {loading && <Spinner />}
             <Container>
                 {   drafts.map((draft, index) => {
