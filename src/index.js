c958d5d1fe7309e55403a4a2aca39f1f42137e15
b-rad-c <bradcorlett@gmail.com>
2022-04-20 23:30:46 -0700

    integration cardano transactions

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ const defaultSettings = {
   ipfsHost: 'http://127.0.0.1:5001',
   draftFolder: '/Users/folder',
   dBranchPublishedDir: '/dBranch/published',
-  wireChannel: 'dbranch-wire'
+  wireChannel: 'dev-dbranch-wire'
 }
 
 
--- src/routes/files.js
+++ src/routes/files.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Container, Row, Col, Spinner, Alert } from 'react-bootstrap'
+import { Container, Row, Col, Spinner, Alert, Modal, Button, Form } from 'react-bootstrap'
 import { PencilSquare, Send } from 'react-bootstrap-icons'
 import { Link } from 'react-router-dom';
 import { create } from 'ipfs-http-client'
@@ -9,27 +9,23 @@ import { ArticleReaderModal, loadArticleFromIPFS } from 'dbranch-core'
 export default function FilesPage(props) {
 
     //
-    // state variables
+    // globals
     //
 
-    const settings = props.settings
-
-    const [loading, setLoading] = useState(true)
-    const [drafts, setDrafts] = useState([])
-    const [publishedDocs, setPublishedDocs] = useState([])
     const [ipfsErrorMsg, setIpfsErrorMsg] = useState('')
     const [localErrorMsg, setLocalErrorMsg] = useState('')
 
-    const [selectedArticleName, setSelectedArticleName] = useState(null)
-    const [selectedArticle, setSelectedArticle] = useState(null)
-    const [showArticle, setShowArticle] = useState(false)
-
+    const settings = props.settings
     const getPublishedPath = (filename) => window.dBranch.joinPath(props.settings.dBranchPublishedDir, filename)
 
 
     //
     // load file listings (run automatically on first render)
     //
+
+    const [loading, setLoading] = useState(true)
+    const [drafts, setDrafts] = useState([])
+    const [publishedDocs, setPublishedDocs] = useState([])
 
     async function loadIPFSDocs() {
         console.log('listing published documents...')
@@ -71,28 +67,13 @@ export default function FilesPage(props) {
     // open an article from ipfs 
     //
 
+    const [selectedArticleName, setSelectedArticleName] = useState(null)
+    const [selectedArticle, setSelectedArticle] = useState(null)
+    const [showArticle, setShowArticle] = useState(false)
+
     const openArticle = (e, name) => { setSelectedArticleName(name); e.preventDefault() }
     const closeArticle = () => { setShowArticle(false) }
     const cleanUpArticle = () => { setSelectedArticleName(null); setSelectedArticle(null) }
-
-    const notifyWire = (name) => {
-        const localPath = getPublishedPath(name)
-
-        const client = create(props.settings.ipfsHost)
-
-        client.files.stat(localPath)
-            .then((stat) => {
-                const payload = {name: name, cid: stat.cid.toString()}
-                console.log(payload)
-                client.pubsub.publish(props.settings.wireChannel, JSON.stringify(payload))
-            }).catch((error) => {
-                console.error(error)
-                setIpfsErrorMsg(error.toString())
-            })
-
-
-        
-    }
 
     useEffect(() => {
         if(selectedArticleName !== null) {
@@ -113,8 +94,37 @@ export default function FilesPage(props) {
         // eslint-disable-next-line
     }, [selectedArticleName])
 
+    //
+    // publish article to news wire
+    //
 
-    const label = 'files'
+    const [showNotifyWireModal, setShowNotifyWireModal] = useState(false)
+    const [articleNameForWire, setArticleNameForWire] = useState(null)
+    const [transactionIdForWire, setTransactionIdForWire] = useState('')
+
+    const openNotificationModal = (name) => { setArticleNameForWire(name); setShowNotifyWireModal(true) }
+    const closeNotifyWireModal = () => { setShowNotifyWireModal(false) }
+    const cleanUpNotifyWireModal = () => { setArticleNameForWire(null) }
+
+    const notifyWire = () => {
+        const localPath = getPublishedPath(articleNameForWire)
+        const client = create(settings.ipfsHost)
+
+        client.files.stat(localPath)
+            .then((stat) => {
+                const payload = {name: articleNameForWire, cid: stat.cid.toString()}
+                if (transactionIdForWire) {
+                    payload.transaction_id = transactionIdForWire
+                }
+                console.log(payload)
+                client.pubsub.publish(settings.wireChannel, JSON.stringify(payload))
+            }).catch((error) => {
+                console.error(error)
+                setIpfsErrorMsg(error.toString())
+            })
+    }
+
+    
     return (
     <main>
 
@@ -138,11 +148,47 @@ export default function FilesPage(props) {
             modalTitle={selectedArticleName} 
             article={selectedArticle} 
             />
+        
+        {/* notify wire modal */}
+        <Modal size='lg' show={showNotifyWireModal} onHide={closeNotifyWireModal} onExited={cleanUpNotifyWireModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>Confirm wire notification</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Row>
+                        <Col sm='3'><strong>wire channel :: </strong></Col><Col>{settings.wireChannel}</Col>
+                    </Row>
+                    <Row>
+                        <Col sm='3'><strong>article name :: </strong></Col><Col>{articleNameForWire}</Col>
+                    </Row>
+                    
+                    <Form.Group as={Row} controlId="transaction-id">
+                        <Form.Label  sm='3' column><strong>transaction&nbsp;&nbsp;::&nbsp;</strong></Form.Label>
+                        <Col>
+                            <Form.Control 
+                                type='text' 
+                                className='text-start'
+                                placeholder='optional cardano transaction id' 
+                                value={transactionIdForWire} 
+                                onChange={(e) => setTransactionIdForWire(e.target.value)}
+                                />
+                        </Col>
+                    </Form.Group>
+                </Form>
+                    
+                
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="warning" onClick={closeNotifyWireModal}>Cancel</Button>
+                <Button variant="success" onClick={notifyWire} >Confirm</Button>
+            </Modal.Footer>
+        </Modal>
 
         {/* list published ipfs files */}
 
         <div className='content'>
-            <p className='inline-header'><strong>published :: </strong>{publishedDocs.length} {label}</p>
+            <p className='inline-header'><strong>published :: </strong>{publishedDocs.length} files</p>
             <Container>
                 {publishedDocs.length === 0 && <p><b>no files found</b></p>}
                 {loading && <Spinner />}
@@ -150,7 +196,7 @@ export default function FilesPage(props) {
                     publishedDocs.map((doc, index) => { return (
                     <Row key={index}>
                         <Col>
-                            <Send size={19} className='published-file-send-icon' onClick={() => notifyWire(doc)} />
+                            <Send size={19} className='published-file-send-icon' onClick={() => openNotificationModal(doc)} />
                             &nbsp;
                             <a href={doc} className='file-link' onClick={(e) => openArticle(e, doc)}>{doc}</a>
                         </Col>
@@ -160,7 +206,7 @@ export default function FilesPage(props) {
 
             {/* list local drafts */}
 
-            <p className='inline-header mt-4'><strong>drafts :: </strong>{drafts.length} {label}</p>
+            <p className='inline-header mt-4'><strong>drafts :: </strong>{drafts.length} files</p>
             <Container>
                 {drafts.length === 0 && <p><b>no files found</b></p>}
                 {   drafts.map((draft, index) => {
